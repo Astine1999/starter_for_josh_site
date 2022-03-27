c6cfb275b5dbcb76e97aa09c4780a7fd9ebf17aa
Astine1999 <aspoestertjie3@gmail.com>
2022-03-27 18:38:14 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -22,3 +22,14 @@ register_nav_menus( array(
     'primary' => __( 'Primary Menu', 'top-menu' ),
 ) );
 ?>
+<?php
+function register_navwalker(){
+  require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+add_action( 'after_setup_theme', 'register_navwalker' );
+?>
+<?php
+register_nav_menus( array(
+    'primary' => __( 'Primary Menu', 'top-menu' ),
+) );
+?>
