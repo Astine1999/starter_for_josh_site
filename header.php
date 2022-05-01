48793277856e125995a3f4c5b3bc946734c748d8
Astine1999 <aspoestertjie3@gmail.com>
2022-05-01 18:14:19 +0800

    Update header.php
    removed style css

--- header.php
+++ header.php
@@ -10,8 +10,8 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@800&display=swap" rel="stylesheet"> 
 
-<link rel="stylesheet" href="style.css">
-<link rel="stylesheet" href="responsive.css" />
+<!-- <link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="responsive.css" /> -->
 <title>   <?php
               if (function_exists('is_tag') && is_tag()) {
                  single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
